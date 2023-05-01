ca17a8022c98d0bc30980018aa54f635f35230b5
kclown0 <keumdy0@gmail.com>
2023-05-01 11:24:42 +0900

    feat: Define print_hello() (Trailing comma test)
    print hello to all names in names.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-# Test ptrhon env
-print('hello')
+# Test python env
+def print_hello():
+    animals = ['dog','cat','hamster'] # in one line
+    foods = [
+        'Spagetti',
+        'Pizza'
+    ] # w/o trailing comma
+    names = [
+        'John',
+        'Jane',
+        'Gil-dong',
+    ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()
+
